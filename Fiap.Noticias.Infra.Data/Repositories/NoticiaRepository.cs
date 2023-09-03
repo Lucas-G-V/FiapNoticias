0db6c8e175118c004d4cc408a7675aef98c19570
Lucas-G-V <79606629+Lucas-G-V@users.noreply.github.com>
2023-09-03 01:40:18 -0300

    Removendo diretivas desnecessarias

--- Fiap.Noticias.Infra.Data/Repositories/NoticiaRepository.cs
+++ Fiap.Noticias.Infra.Data/Repositories/NoticiaRepository.cs
@@ -2,11 +2,6 @@
 using Fiap.Noticias.Domain.Model.Entities;
 using Fiap.Noticias.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fiap.Noticias.Infra.Data.Repositories
 {
